3621717dea28f74e5bc9516b3e440fb76b26ebe8
manojb99ccna <manojb99.ccna@gmail.com>
2022-04-26 19:13:31 +0530

    updateddd

--- index.php
+++ index.php
@@ -2,8 +2,46 @@
   
 /*   extends AnotherClass implements Interface */
 
+class ClassName 
+{
+	public $a,$b,$c;
 
-echo "22";
+	function __construct( $a = 0, $b=0)
+	{
+		
+echo "step 3 <Br>";
+
+
+		$this->a = $a;
+		$this->b = $b;
+
+	}
+
+	public function sum(){
+		return $this->a+$this->b;
+	}
+
+	public function sum2(){
+		return $this->a+$this->b;
+	}
+
+
+	function __destruct()
+	{
+		
+		echo "step 3 <Br>";
+	} 
+ 
+	
+}
+
+$ClassName = new ClassName(6,7);
+ 
+echo $ClassName->sum();
+echo " <Br>";
+echo " <Br>";
+echo $ClassName->sum2();
+ 
 
 
 ?>
